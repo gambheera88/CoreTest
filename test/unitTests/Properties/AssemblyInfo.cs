c8040afc3c44dddbad5e1fdfd70d489afbdfd763
Lahiru Gambheera <Lahiru.Gambheera@degani.com.au>
2017-07-18 08:49:39 +0530

    .net unit test

---
+++ test/unitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unitTests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4cf2ea13-34d3-4620-b290-88f268521baf")]
---
+++ test/unitTests/ValuesController_Get.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core_Sample.Controllers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace unitTests
+{
+    public class ValuesController_Get
+    {
+        [Fact]
+        public void ShouldReturnTwoValues()
+        {
+            var valController = new ValuesController();
+            var result = valController.Get();
+
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
